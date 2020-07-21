5f4cbd4ab30caced319ff37c12be4ba62f49f143
Bluebird <stephany1445@hotmail.com>
2020-07-20 20:34:18 -0700

    Added a While loop that makes user enter a number between 8-128 added parseInt to acquire a number from user in the input

--- Develop/script.js
+++ Develop/script.js
@@ -12,11 +12,17 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Option Arrays the user may choose from
+// Option StringArrays the user may choose from
 var number = ["0 1 2 3 4 5 6 7 8 9"];
 var specialCharacter = ["! # $ % & * + - . / : ; < = > ? @ [ ] ^ _ { | } ~"];
 var lwrCase = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"];
 var uppCase = ["A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"];
+
+// Splitting the String Arrays
+var splitNum = number.split(" ");
+var splitSpChar = specialCharacter.split(" ");
+var splitLwrCase = lwrCase.split(" ");
+var splitUppCase = uppCase.split(" ");
 
 // Confirm options the user agrees to
 var confirmSpChar = confirm(
@@ -37,7 +43,12 @@ function generatePassword() {
       "Please choose the length of the password you would like for it to be."
     )
   );
-  if (confirmLength <= 7 || confirmLength >= 128) {
-    alert("Please try again. Password must be at least 7 characters long");
+  while (confirmLength <= 7 || confirmLength >= 129) {
+    alert("Please try again. Password must be between 8-128 characters long");
+    var confirmLength = parseInt(
+      prompt(
+        "Please choose the length of the password you would like for it to be."
+      )
+    );
   }
 }
