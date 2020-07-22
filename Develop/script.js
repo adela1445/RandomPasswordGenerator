dd080ed58610cf3eada4537e504fb82bc853198f
Bluebird <stephany1445@hotmail.com>
2020-07-21 18:38:03 -0700

    removed brackets from my string in order to split Added if statements if certain criteria was asked to add created a character container to put all character that user is confirming to use created a return container of where the random pass will exist added console log to check if if statements are working

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Option StringArrays the user may choose from
-var number = ["0 1 2 3 4 5 6 7 8 9"];
-var specialCharacter = ["! # $ % & * + - . / : ; < = > ? @ [ ] ^ _ { | } ~"];
-var lwrCase = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"];
-var uppCase = ["A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"];
+var number = "0123456789";
+var specialCharacter = "!#$%&*+-./:;<=>?@[]^_{|}~";
+var lwrCase = "abcdefghijklmnopqrstuvwxyz";
+var uppCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Splitting the String Arrays
-var splitNum = number.split(" ");
-var splitSpChar = specialCharacter.split(" ");
-var splitLwrCase = lwrCase.split(" ");
-var splitUppCase = uppCase.split(" ");
+
+var splitNum = number.split("");
+var splitSpChar = specialCharacter.split("");
+var splitLwrCase = lwrCase.split("");
+var splitUppCase = uppCase.split("");
+console.log(splitNum);
+console.log(splitSpChar);
+console.log(splitLwrCase);
+console.log(splitUppCase);
 
 // User will be asked the password length
 function generatePassword() {
@@ -71,4 +68,44 @@ function generatePassword() {
     );
     var confirmNum = confirm("Press 'OK' if you would like to use Numbers?");
   }
+
+  // My character container
+  var characterCont = [];
+
+  // The result of random password
+  var randomPass = [];
+
+  // Adding the characters based on user confirming
+  if (confirmSpChar == true) {
+    console.log(characterCont);
+
+    characterCont = characterCont.concat(splitSpChar);
+  }
+  if (confirmNum == true) {
+    characterCont = characterCont.concat(splitNum);
+  }
+
+  if (confirmUppCase == true) {
+    characterCont = characterCont.concat(splitUppCase);
+  }
+
+  if (confirmLwrCase == true) {
+    characterCont = characterCont.concat(splitLwrCase);
+  }
+
+  console.log(characterCont);
+
+  for (var i = 0; i < confirmLength; i++) {
+    randomPass =
+      randomPass +
+      characterCont[Math.floor(Math.random() * characterCont.length)];
+  }
+  return randomPass;
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
